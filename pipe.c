a7e84cc8e3a80c2817935bb1c3c20bf7798e8e98
jcavalca <jcavalca@calpoly.com>
2020-11-26 10:31:21 -0800

    i/o redirection working

--- pipe.c
+++ pipe.c
@@ -7,12 +7,22 @@
 
 
 int input_redirection(int stage, char *input){
-	int ret = -1;
+	int ret = FALSE;
 	/* Input Redirection */
 	if (stage == 0 && strcmp(input, "original stdin") != 0){
-	    ret = 1;
+	    ret = TRUE;
 	}
+	return ret;
+}
 
+/* test for the only two possible scenarios of output redirection */
+int output_redirection(int stage, int numb_pipes, char *output){
+	int ret = FALSE;
+	if ( stage == 0 && numb_pipes == 0 && 
+		strcmp(output, "pipe to stage 1") != 0)
+              ret = TRUE;
+	else if (stage == numb_pipes - 1 && strcmp(output, "original stdout") != 0)		
+	      ret = TRUE;
 	return ret;
 }
 
@@ -39,19 +49,20 @@ int pipe_stages(Stage *stageArr[COMM_LEN_MAX], int numb_pipes){
 	  if (!(child = fork())){
     	  /* child */
 	    int fd_in, fd_out;
-	    int test_in;
+	    int test_in, test_out;
 	    int redir_in = FALSE;
 	    fd_in = STDIN_FILENO; /* default */
 	    fd_out = STDOUT_FILENO;
-	    if (-1 != (test_in = input_redirection(i, stageArr[i] -> input)))
+
+	    /* read end */
+	    if (TRUE == (test_in = input_redirection(i, stageArr[i] -> input)))
 	    {
 		if (-1 == (test_in = open(stageArr[i] -> input, O_RDONLY))){
 		    perror(stageArr[i] -> input);
 		    exit(EXIT_FAILURE);
 		}
-	/*	dup2(test_in, fd_in);*/	
 	    }
-	    if (test_in == -1){
+	    if (test_in == FALSE){
 	        if( -1 == dup2(old[READ_END], fd_in) ){
 		     perror("dup2 old");	
 	          }
@@ -61,18 +72,33 @@ int pipe_stages(Stage *stageArr[COMM_LEN_MAX], int numb_pipes){
                   }
 		redir_in = TRUE;
 	   }
+
+	   /* write end */	
+	    if (TRUE == (test_out = output_redirection(i, numb_pipes,  stageArr[i] -> output))){
+		if (-1 == (test_out = open(stageArr[i] -> output, 
+			              O_WRONLY | O_CREAT | O_TRUNC, 
+	   		              (S_IWUSR | S_IRUSR)))){
+                    perror(stageArr[i] -> output);
+                    exit(EXIT_FAILURE);
+                }
+		if( -1 == dup2(test_out, STDOUT_FILENO) ){
+                     perror("dup2 old");
+                  }
+                redir_in = TRUE;
+            }
+	   
+
 	    if( i < num - 1){
-	      /* output redirection */
-              if ( -1 == dup2(next[WRITE_END], fd_out)){
+              if ( -1 == dup2(next[WRITE_END], STDOUT_FILENO)){
 		perror("dup2 new");
 		}
 	    }
 	     /* cleaning parent's opened fd's */
 	     close(old[READ_END]);
 	     close(old[WRITE_END]);
-	     if (! redir_in){
-	     close(next[READ_END]);		
-  	     close(next[WRITE_END]);}
+	     if (redir_in == FALSE){
+	         close(next[READ_END]);		
+  	         close(next[WRITE_END]);}
 	     exec_command(i, stageArr[i] -> argc, stageArr[i] -> argv);
 	     exit(EXIT_FAILURE); /* exec failed */
 	}
